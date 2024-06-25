5d74f6d7c58d4f394715875d09ec7e65176728df
Altamash9146 <sayyedaltamash853@gmail.com>
2024-06-25 18:56:06 +0530

    cors modified

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin:"*"
-}))
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204
+}));
 
 const generateInvoice = require("./Routes/generateInvoiceRoutes");
 app.use("/api", generateInvoice);
